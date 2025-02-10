22af9c8a3602f2af10e97857a9c0fdd711597b70
uyr83157 <uyr83157@gmail.com>
2025-02-10 14:32:26 +0900

    feat: Entity 추가

---
+++ src/main/java/com/example/board/entity/Member.java
@@ -0,0 +1,22 @@
+package com.example.board.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Integer age;
+
+}
